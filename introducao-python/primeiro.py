abd665c4f24f4b372bfbf25ef03598be9018bea9
Bárbara Arruda <65787282+barbaraarruda@users.noreply.github.com>
2024-12-06 19:44:09 -0300

    comentários demonstração

--- introducao-python/primeiro.py
+++ introducao-python/primeiro.py
@@ -4,3 +4,9 @@ print('Hello World!')
 print('Seja bem vinda ao curso de Python do Coding Tank!')
 
 input('Qual é a linguagem de programação que você está estudando?')
+
+"""
+Esse é um comentário
+Esse ainda é um comentário
+de várias linhas
+"""
